ae5bde7fcf9ea27c35d495a56cef53b2e102e469
austingere <122937592+austingere@users.noreply.github.com>
2023-02-07 15:52:01 -0600

    Update finalize.php

--- bpareg-old/finalize.php
+++ bpareg-old/finalize.php
@@ -7,7 +7,7 @@ body {
 </style>
 <title>SLC Registration</title>
 <body text="#000000" link="#FF0000" vlink="#FF0000">
-<? 
+<?php 
 $school_number=$_GET['school_number'];
 
 if ($school_number) {  include ('finalize-text.php'); }
