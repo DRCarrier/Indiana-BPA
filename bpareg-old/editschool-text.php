5544679454a633e712dfa41066d0ee18dcafc6b1
austingere <122937592+austingere@users.noreply.github.com>
2023-02-07 15:48:30 -0600

    Update editschool-text.php

--- bpareg-old/editschool-text.php
+++ bpareg-old/editschool-text.php
@@ -7,13 +7,13 @@ body {
 </style>
 <title>SLC Registration</title>
 <body text="#000000" link="#FF0000" vlink="#FF0000">
-<? 
+<?php 
 $school_number=$_GET['school_number'];
 
 ?>
 
 
-<?
+<?php
 
 $con = mysql_connect('server','username','password');
 $db = mysql_select_db('database', $con);
