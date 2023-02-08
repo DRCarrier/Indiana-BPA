955865bab144abc3b79e33f7d7b9b5abddc9ebb6
DRCarrier <113132467+DRCarrier@users.noreply.github.com>
2023-02-07 18:56:07 -0600

    Update init_login.php

--- bpareg-old/init_login.php
+++ bpareg-old/init_login.php
@@ -1,69 +1,138 @@
-<html>
-
-<head>
-<meta http-equiv="Content-Language" content="en-us">
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<meta name="GENERATOR" content="Microsoft FrontPage 4.0">
-<meta name="ProgId" content="FrontPage.Editor.Document">
-<title>Conference Registration</title>
-<link href="ibeareg_style.css" rel="stylesheet" type="text/css">
-<link href="style_sheet.css" rel="stylesheet" type="text/css"></head>
-
-<body bgcolor="#FFFFFF" text="#000000" link="#0000FF" vlink="#0000FF" alink="#0000FF">
-<form name="form1" method="post" action="proc_init_login.php">
-  <p>&nbsp;</p>
-  <table width="87%" align="center">
-    <tr>
-      <td width="23%" height="77"><img src="bpa-logo.jpg" alt="BPA Logo" width="400" height="134"></td>
-      <td width="77%"><p class="text_title"><font size="+2" face="Arial, Helvetica, sans-serif"><strong>2020  
-        Indiana Business Professionals of America</strong></font></p>
-        <p class="text_title"><strong><font size="+2" face="Arial, Helvetica, sans-serif">Online Conference  
-      Registration</font></strong></p></td>
-    </tr>
-  </table>
-  <p>&nbsp;</p>
-  <table width="100%" border="0">
-    <tr>
-      <td><font size="3" class="heading"><span class="text">Before using the 2020 Online Conference Registration system, you will need to create an account. Your username is your school number. If you are not sure of your school number, open the <a href="school-list.pdf" target="_blank">School List file</a> to locate your school number. </span></font></td>
-    </tr>
-    <tr>
-      <td height="232" valign="top"><p align="left">&nbsp;</p>
-        <p align="left"><strong>Create New Account:</strong></p>
-        <table width="84%" border="0">
-          <tr>
-            <td width="20%"><span class="para_text"><font size="2" class="text">Username:</font></span></td>
-            <td width="22%"><span class="para_text">
-              <input name="username" type="text" id="username" size="20" maxlength="12">
-            </span></td>
-            <td width="58%"><span class="para_text"><font size="2" class="text">(School Number - See <a href="school-list.pdf" target="_blank">School List file</a> to locate your school number)</font></span></td>
-          </tr>
-          <tr>
-            <td><span class="para_text"><font size="2" class="text">Password:</font></span></td>
-            <td><span class="para_text">
-              <input name="password" type="password" id="password" size="20" maxlength="12">
-            </span></td>
-            <td><span class="para_text"><font size="2" class="text">(6-12 characters)</font></span></td>
-          </tr>
-          <tr>
-            <td><span class="para_text"><font size="2" class="text">Confirm Password:</font></span></td>
-            <td><span class="para_text">
-              <input name="password2" type="password" id="password2" size="20" maxlength="12">
-            </span></td>
-            <td>&nbsp;</td>
-          </tr>
-        </table>
-        <p align="left" class="para_text"><font face="Arial, Helvetica, sans-serif">
-          <input type="submit" value="Submit" name="B1">
-        </font></p>
-<p align="left"></p>
-      <p align="left"></p></td>
-    </tr>
-  </table>
-</form>
-<p align="left">&nbsp;</p>
-<p align="left">&nbsp;</p>
-<h2 align="center">&nbsp;</h2>
-
-</body>
-
+// Log in 
+<html>
+
+
+
+<head>
+
+<meta http-equiv="Content-Language" content="en-us">
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<meta name="GENERATOR" content="Microsoft FrontPage 4.0">
+
+<meta name="ProgId" content="FrontPage.Editor.Document">
+
+<title>Conference Registration</title>
+
+<link href="ibeareg_style.css" rel="stylesheet" type="text/css">
+
+<link href="style_sheet.css" rel="stylesheet" type="text/css"></head>
+
+
+
+<body bgcolor="#FFFFFF" text="#000000" link="#0000FF" vlink="#0000FF" alink="#0000FF">
+
+<form name="form1" method="post" action="proc_init_login.php">
+
+  <p>&nbsp;</p>
+
+  <table width="87%" align="center">
+
+    <tr>
+
+      <td width="23%" height="77"><img src="bpa-logo.jpg" alt="BPA Logo" width="400" height="134"></td>
+
+      <td width="77%"><p class="text_title"><font size="+2" face="Arial, Helvetica, sans-serif"><strong>2020  
+
+        Indiana Business Professionals of America</strong></font></p>
+
+        <p class="text_title"><strong><font size="+2" face="Arial, Helvetica, sans-serif">Online Conference  
+
+      Registration</font></strong></p></td>
+
+    </tr>
+
+  </table>
+
+  <p>&nbsp;</p>
+
+  <table width="100%" border="0">
+
+    <tr>
+
+      <td><font size="3" class="heading"><span class="text">Before using the 2020 Online Conference Registration system, you will need to create an account. Your username is your school number. If you are not sure of your school number, open the <a href="school-list.pdf" target="_blank">School List file</a> to locate your school number. </span></font></td>
+
+    </tr>
+
+    <tr>
+
+      <td height="232" valign="top"><p align="left">&nbsp;</p>
+
+        <p align="left"><strong>Create New Account:</strong></p>
+
+        <table width="84%" border="0">
+
+          <tr>
+
+            <td width="20%"><span class="para_text"><font size="2" class="text">Username:</font></span></td>
+
+            <td width="22%"><span class="para_text">
+
+              <input name="username" type="text" id="username" size="20" maxlength="12">
+
+            </span></td>
+
+            <td width="58%"><span class="para_text"><font size="2" class="text">(School Number - See <a href="school-list.pdf" target="_blank">School List file</a> to locate your school number)</font></span></td>
+
+          </tr>
+
+          <tr>
+
+            <td><span class="para_text"><font size="2" class="text">Password:</font></span></td>
+
+            <td><span class="para_text">
+
+              <input name="password" type="password" id="password" size="20" maxlength="12">
+
+            </span></td>
+
+            <td><span class="para_text"><font size="2" class="text">(6-12 characters)</font></span></td>
+
+          </tr>
+
+          <tr>
+
+            <td><span class="para_text"><font size="2" class="text">Confirm Password:</font></span></td>
+
+            <td><span class="para_text">
+
+              <input name="password2" type="password" id="password2" size="20" maxlength="12">
+
+            </span></td>
+
+            <td>&nbsp;</td>
+
+          </tr>
+
+        </table>
+
+        <p align="left" class="para_text"><font face="Arial, Helvetica, sans-serif">
+
+          <input type="submit" value="Submit" name="B1">
+
+        </font></p>
+
+<p align="left"></p>
+
+      <p align="left"></p></td>
+
+    </tr>
+
+  </table>
+
+</form>
+
+<p align="left">&nbsp;</p>
+
+<p align="left">&nbsp;</p>
+
+<h2 align="center">&nbsp;</h2>
+
+
+
+</body>
+
+
+
 </html>
