b52cf403e4c63f0236060d3ab2ab048d4a2a2a61
mnetienne <122938286+mnetienne@users.noreply.github.com>
2023-02-16 13:20:50 -0600

    Update proc_editschool.php

--- bpareg-old/proc_editschool.php
+++ bpareg-old/proc_editschool.php
@@ -20,12 +20,17 @@ $quality=$_POST['quality'];
 $other_recog=$_POST['other_recog'];
 $accommodations=$_POST['accommodations'];
 
-$con = mysql_connect('server','username','password');
-$db = mysql_select_db('database', $con);
+//Connection to DB using PDO:
+$servername = "localhost";
+$username = "bpareg";
+$password = "Planetary533TrollOhm";
+
+$con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
+$con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $sql = "UPDATE SLCschl SET address='$address', city='$city', zip='$zip', arrive='$arrive', depart='$depart',  overflowSH='$overflowSH', envact='$envact', community='$community', olympics='$olympics', safety='$safety', chact='$chact', service='$service', member='$member', quality='$quality', social='$social', marketing='$marketing', other_recog='$other_recog', accommodations='$accommodations', UpdateDate=NOW() WHERE school_number='$school_number'"; 
 
-$query = mysql_query($sql) or die(mysql_error());
+$query = $con->query($sql) or die($con->errorInfo());
 
 header ("Location: /SLCReg/index.php?school_number=$school_number");
 
