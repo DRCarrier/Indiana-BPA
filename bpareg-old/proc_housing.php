05d1919296f842844826bfda3120adfd63501265
austingere <122937592+austingere@users.noreply.github.com>
2023-02-16 14:01:30 -0600

    new db connection

--- bpareg-old/proc_housing.php
+++ bpareg-old/proc_housing.php
@@ -11,8 +11,13 @@ $occ_id4=$_POST['occ_id4'];
 $finished=$_POST['finished'];
 $addanother=$_POST['addanother'];
 
-$con = mysql_connect('server','username','password');
-$db = mysql_select_db('database', $con);
+$servername = "localhost";
+$username = "bpareg";
+$password = "Planetary533TrollOhm";
+
+$con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
+$con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 $sql = "INSERT INTO housing (`school_number`, `a_s`, `gender`, `type`, `occ_id1`, `occ_id2`, `occ_id3`, `occ_id4`) VALUES ('$school_number', '$a_s', '$gender', '$type', '$occ_id1', '$occ_id2', '$occ_id3', '$occ_id4');";
 
