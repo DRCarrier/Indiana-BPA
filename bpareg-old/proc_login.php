c198a1b9768b55788396cf864790d196e97d4297
austingere <122937592+austingere@users.noreply.github.com>
2023-02-23 15:42:53 -0600

    Update proc_login.php

--- bpareg-old/proc_login.php
+++ bpareg-old/proc_login.php
@@ -1,46 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>SLC Registration</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>SLC Registration</title>
 </head>
 <body>
- 
- <?php $password3=$_POST['password3'];
-$username=$_POST['username'];
-$school_number=$_POST['username'];
-?>
+    
+    <?php
+    $password3 = $_POST['password3'];
+    $username = $_POST['username'];
+    $school_number = $_POST['school_number']; 
 
-<form name="form1" method="post" action="index.php<?php echo "?school_number=$school_number"; ?>">
-<?php
-$servername = "localhost";
-$username = "bpareg";
-$password = "Planetary533TrollOhm";
+    // establish database connection
+    $servername = "localhost";
+    $dbusername = "bpareg"; 
+    $dbpassword = "Planetary533TrollOhm";
+    $dbname = "bpareg"; // fixed: added the database name to be used
+    $con = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
-$con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // perform update query
+    $query = "UPDATE `login` SET `password3` =  :password WHERE `username` = :username";
+    $stmt = $con->prepare($query);
+    $stmt->bindParam(':password', $password3);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
 
+    // fetch login details for verification
+    $sql2 = "SELECT * FROM login WHERE username = :username";
+    $stmt2 = $con->prepare($sql2);
+    $stmt2->bindParam(':username', $username);
+    $stmt2->execute();
+    $login = $stmt2->fetch(PDO::FETCH_ASSOC);
 
-$query = "UPDATE `login` SET `password3` =  '$password3' WHERE `username`='$username'";
-
- 
-$con->query($query);
-
-$sql2 = "SELECT * FROM login WHERE username = '$username'";
-
-$query2 = $con->query($sql2) or die($con->errorInfo());
-$login = $query2->fetch(PDO::FETCH_BOTH); 
-
-if ($login['username'] == $username && $login['password'] == $password3) {
-    echo '<h2 align="left"><font face="Arial, Helvetica, sans-serif">Click the Next button to continue to your registration.</font></h2>';
-
-	
-	echo "<input type=\"submit\" value=\"Next\" name=\"Next\" id=\"Submit\" />"; }
-	
-	else 
-
-	{echo '<font face="Arial, Helvetica, sans-serif"><b>The username and/or password do not match what we have in our records.  Please click the BACK button on your browser and try again.  If you do not remember your username and/or password email <a href="mailto:mccloudtr@mvschool.org">Tina McCloud</a>'; }
-?>
-</form>
+    // check if login details are correct
+    if ($login && $login['username'] == $username && $login['password3'] == $password3) {
+        echo '<h2 align="left"><font face="Arial, Helvetica, sans-serif">Click the Next button to continue to your registration.</font></h2>';
+        echo '<form name="form1" method="post" action="index.php">';
+        echo "<input type=\"hidden\" name=\"school_number\" value=\"$school_number\">";
+        echo "<input type=\"submit\" value=\"Next\" name=\"Next\" id=\"Submit\" />";
+        echo '</form>';
+    } else {
+        echo '<font face="Arial, Helvetica, sans-serif"><b>The username and/or password do not match what we have in our records. Please click the BACK button on your browser and try again. If you do not remember your username and/or password email <a href="mailto:mccloudtr@mvschool.org">Tina McCloud</a></b></font>';
+    }
+    ?>
 </body>
 </html>
