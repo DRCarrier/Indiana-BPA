d781e299af60865a9350ff8593abe959a46f9623
DRCarrier <113132467+DRCarrier@users.noreply.github.com>
2023-03-21 15:52:04 -0500

    Update proc_login.php
    echo variables for help

--- bpareg-old/proc_login.php
+++ bpareg-old/proc_login.php
@@ -73,6 +73,11 @@
 
     } else {
         echo '<font face="Arial, Helvetica, sans-serif"><b>The username and/or password do not match what we have in our records. Please click the BACK button on your browser and try again. If you do not remember your username and/or password email <a href="mailto:mccloudtr@mvschool.org">Tina McCloud</a></b></font>';
+        echo '$username';
+        echo '$password3';
+        
+        
+        
     }
     ?>
 </body>
