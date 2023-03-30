01b368ddd20e4bd1571c9969d8a9de378e9bd35e
DRCarrier <113132467+DRCarrier@users.noreply.github.com>
2023-03-30 15:56:34 -0500

    Update studentlist.php

--- bpareg-old/studentlist.php
+++ bpareg-old/studentlist.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+print_r($_SESSION);
 $school_number = $_SESSION['school_number'];
 $delete = isset($_GET['delete']) ? true : false;
 $id_num = isset($_GET['id_num']) ? $_GET['id_num'] : 0;
