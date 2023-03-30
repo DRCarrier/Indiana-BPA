dd2b3cccc4bc659c46f592da1d709aa7004d67d1
mnetienne <122938286+mnetienne@users.noreply.github.com>
2023-03-30 15:44:37 -0500

    Update housinglist .php

--- bpareg-old/housinglist .php
+++ bpareg-old/housinglist .php
@@ -29,6 +29,7 @@ $sql = "SELECT school_name FROM schools WHERE school_number = '$school_number'";
 $result = $con->query($sql) or die($con->errorInfo());
 $temp = $result->fetch(PDO::FETCH_ASSOC);
 
+print $school_number;
 print $temp;
 $school_name = $temp[school_name];
 
