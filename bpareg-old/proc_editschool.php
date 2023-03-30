d4ea86f1eecf6fbebc97d85bfb313d222dbdf3f6
mnetienne <122938286+mnetienne@users.noreply.github.com>
2023-03-29 20:56:41 -0500

    Removed unused variables

--- bpareg-old/proc_editschool.php
+++ bpareg-old/proc_editschool.php
@@ -4,25 +4,27 @@ $school_number=$_SESSION['school_number'];
 $address=$_POST['address'];
 $city=$_POST['city'];
 $zip=$_POST['zip'];
-$arrive=$_POST['arrive'];
-$depart=$_POST['depart'];
 
-$overflowSH=$_POST['overflowSH'];
 $envact=$_POST['envact'];
 $community=$_POST['community'];
 $olympics=$_POST['olympics'];
 $social=$_POST['social'];
 $member=$_POST['member'];
 $safety=$_POST['safety'];
-$chact=$_POST['chact'];
 $service=$_POST['service'];
 $marketing=$_POST['marketing'];
 $quality=$_POST['quality'];
-$other_recog=$_POST['other_recog'];
 $accommodations=$_POST['accommodations'];
 
-//$con = mysql_connect('server','username','password');
-//$db = mysql_select_db('database', $con);
+/* Removed variables:
+$arrive=$_POST['arrive'];
+$depart=$_POST['depart'];
+$overflowSH=$_POST['overflowSH'];
+$chact=$_POST['chact'];
+$other_recog=$_POST['other_recog'];
+*/
+
+//Connection to DB using PDO:
 $servername = "localhost";
 $username = "bpareg";
 $password = "Planetary533TrollOhm";
@@ -31,9 +33,16 @@ $con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
 $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-$sql = "UPDATE SLCschl SET address='$address', city='$city', zip='$zip', arrive='$arrive', depart='$depart',  overflowSH='$overflowSH', envact='$envact', community='$community', olympics='$olympics', safety='$safety', chact='$chact', service='$service', member='$member', quality='$quality', social='$social', marketing='$marketing', other_recog='$other_recog', accommodations='$accommodations', UpdateDate=NOW() WHERE school_number='$school_number'"; 
+$sql = "UPDATE SLCschl SET address='$address', city='$city', zip='$zip',  envact='$envact', community='$community', olympics='$olympics', safety='$safety',  service='$service', member='$member', quality='$quality', social='$social', marketing='$marketing', accommodations='$accommodations', UpdateDate=NOW() WHERE school_number='$school_number'"; 
 
-//$query = mysql_query($sql) or die(mysql_error());
+/* Removed variables from query:
+arrive='$arrive'
+depart='$depart'
+overflowSH='$overflowSH'
+chact='$chact'
+other_recog='$other_recog'
+*/
+
 query = $con->query($sql) or die($con->errorInfo());
 
 header ("Location: /new/index-text-main.php?school_number=$school_number");
