fe9951191a4b6d72465323b4f8bcf4f1b9631d15
austingere <122937592+austingere@users.noreply.github.com>
2023-04-04 15:23:22 -0500

    Update finalize-text.php

--- bpareg-old/finalize-text.php
+++ bpareg-old/finalize-text.php
@@ -33,7 +33,7 @@ $school = $query->fetch(PDO::FETCH_BOTH);
 <font face=\"Arial, Helvetica, sans-serif\"><h1>Finalize Registration for School #$school[school_number]</font></h1>
 <form method=\"post\" action=\"proc_finalize.php\">
 <table>
-<tr><td>Registration is Complete: </td><td> <input type=\"checkbox\" name=\"completed\" value=\"y\""; if (mysql_num_rows($school) > 0) { echo " checked=\"checked\""; } echo "></td></tr>\n
+<tr><td>Registration is Complete: </td><td> <input type=\"checkbox\" name=\"completed\" value=\"y\""; if (mysqli_num_rows($school) > 0) { echo " checked=\"checked\""; } echo "></td></tr>\n
 <tr><td>Number of Students Registered: </td> <td><input type=\"text\" name=\"students\" value=\"$school[students]\"></td></tr> 
 <tr><td>Number of Advisors Registered: </td><td><input type=\"text\" name=\"advisors\" value=\"$school[advisors]\"></td></tr>
 <tr><td>Number of Chaperones Registered: </td><td><input type=\"text\" name=\"chaperones\" value=\"$school[chaperones]\"></td></tr>
