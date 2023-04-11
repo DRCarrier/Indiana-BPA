575b9d4f5d34ba1a534ba55390c181ee0b8c485a
austingere <122937592+austingere@users.noreply.github.com>
2023-04-11 15:55:42 -0500

    Update proc_login.php

--- bpareg-old/proc_login.php
+++ bpareg-old/proc_login.php
@@ -1,144 +1,106 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>SLC Registration</title>
-</head>
-<body>
-    
-    <?php
-    
-    $password3 = $_POST['password3'];
-    $username = $_POST['username'];
-    $school_number = $_POST['school_number']; // fixed: use correct variable name
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <title>SLC Registration</title>
+	</head>
+	<body>
+	    
+	    <?php
+	    
+	    $password3 = $_POST['password3'];
+	    $username = $_POST['username'];
+	    $school_number = $_POST['username'];
+	   
+	
 
-    // create hash password
-    $password = $password3;
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+	    // create hash password
+	    $password = '$password3';
+	    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+	
 
-    //echo $hashed_password;
+	    echo $hashed_password;
+	
 
-    // establish database connection
-    $servername = "localhost";
-    $dbusername = "bpareg"; 
-    $dbpassword = "Planetary533TrollOhm";
-    $dbname = "bpareg";
-    $con = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
 
-    // fetch login details for verification
-    $sql2 = "SELECT * FROM login WHERE username = :username AND password3 = :password3";
-    $stmt2 = $con->prepare($sql2);
-    $stmt2->bindParam(':username', $username);
-    $stmt2->bindParam(':password3', $hashed_password); // fixed: bind hashed password instead of 'password'
-    $stmt2->execute();
-    $login = $stmt2->fetch(PDO::FETCH_ASSOC);
+	    
+	    
+	    //Verify hashed password
+	    //$password = 'password3';
+	    //$hashed_password = 'password';
+	    
+	    //if (password_verify($password, $hashed_password)) {
+	      //echo 'Password is valid!';
+	   // } else {
+	   //  echo 'Invalid password.';
+	    //}
+	
 
-    // check if login details are correct
-    if ($login && $login['username'] == $username && password_verify($password3, $login['password3'])) { // fixed: verify password using password_verify
-        //Creates a session variable
-        session_start();
-        $_SESSION["school_number"]= $school_number; // fixed: use correct variable name
-        header("Location:/new/index-text-main.php");
+	
 
-    } else {
-        echo '<font face="Arial, Helvetica, sans-serif"><b>The username and/or password do not match what we have in our records. Please click the BACK button on your browser and try again. If you do not remember your username and/or password email <a href="mailto:mccloudtr@mvschool.org">Tina McCloud</a></b></font>';
-        echo $username;
-        echo $password3;
-    }
-    ?>
-</body>
-</html>```
+	
 
+	
 
+	
 
-austingere7@gmail.com
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>SLC Registration</title>
-</head>
-<body>
-    
-    <?php
-    
-    $password3 = $_POST['password3'];
-    $username = $_POST['username'];
-    $school_number = $_POST['username'];
-   
+	
 
-    // create hash password
-    $password = '$password3';
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+	    // establish database connection
+	    $servername = "localhost";
+	    $dbusername = "bpareg"; 
+	    $dbpassword = "Planetary533TrollOhm";
+	    $dbname = "bpareg"; // fixed: added the database name to be used
+	    $con = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+	    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
 
-    echo $hashed_password;
+	    // perform update query
+	  //  $query = "UPDATE `login` SET `password3` =  :password WHERE `username` = :username";
+	   // $stmt = $con->prepare($query);
+	  //  $stmt->bindParam(':password', $password3);
+	   // $stmt->bindParam(':username', $username);
+	   // $stmt->execute();
+	
 
+	    // fetch login details for verification
+	   $sql2 = "SELECT * FROM login WHERE username = :username AND password3 = :password3";
+	$stmt2 = $con->prepare($sql2);
+	$stmt2->bindParam(':username', $username);
+	$stmt2->bindParam(':password3', $password3);
+	$stmt2->execute();
+	$login = $stmt2->fetch(PDO::FETCH_ASSOC);
+	
 
-    
-    
-    //Verify hashed password
-    //$password = 'password3';
-    //$hashed_password = 'password';
-    
-    //if (password_verify($password, $hashed_password)) {
-      //echo 'Password is valid!';
-   // } else {
-   //  echo 'Invalid password.';
-    //}
+	    // check if login details are correct
+	    if ($login && $login['username'] == $username && $login['password3'] == $hashed_password) {
+	        //Creates a session variable
+	    session_start();
+	    $_SESSION["school_number"]= $username;
+	    header("Location:/new/index-text-main.php");
+	        //echo '<h2 align="left"><font face="Arial, Helvetica, sans-serif">Click the Next button to continue to your registration.</font></h2>';
+	        //echo '<form name="form1" method="post" action="index-text-main.php">';
+	        //echo "<input type=\"hidden\" name=\"school_number\" value=\"$school_number\">";
+	        //echo "<input type=\"submit\" value=\"Next\" name=\"Next\" id=\"Submit\" />";
+	        //echo '</form>';
+	
 
+	
 
-
-
-
-
-    // establish database connection
-    $servername = "localhost";
-    $dbusername = "bpareg"; 
-    $dbpassword = "Planetary533TrollOhm";
-    $dbname = "bpareg"; // fixed: added the database name to be used
-    $con = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // perform update query
-  //  $query = "UPDATE `login` SET `password3` =  :password WHERE `username` = :username";
-   // $stmt = $con->prepare($query);
-  //  $stmt->bindParam(':password', $password3);
-   // $stmt->bindParam(':username', $username);
-   // $stmt->execute();
-
-    // fetch login details for verification
-   $sql2 = "SELECT * FROM login WHERE username = :username AND password3 = :password3";
-$stmt2 = $con->prepare($sql2);
-$stmt2->bindParam(':username', $username);
-$stmt2->bindParam(':password3', $password3);
-$stmt2->execute();
-$login = $stmt2->fetch(PDO::FETCH_ASSOC);
-
-    // check if login details are correct
-    if ($login && $login['username'] == $username && $login['password3'] == $hashed_password) {
-        //Creates a session variable
-    session_start();
-    $_SESSION["school_number"]= $username;
-    header("Location:/new/index-text-main.php");
-        //echo '<h2 align="left"><font face="Arial, Helvetica, sans-serif">Click the Next button to continue to your registration.</font></h2>';
-        //echo '<form name="form1" method="post" action="index-text-main.php">';
-        //echo "<input type=\"hidden\" name=\"school_number\" value=\"$school_number\">";
-        //echo "<input type=\"submit\" value=\"Next\" name=\"Next\" id=\"Submit\" />";
-        //echo '</form>';
+	    } else {
+	        echo '<font face="Arial, Helvetica, sans-serif"><b>The username and/or password do not match what we have in our records. Please click the BACK button on your browser and try again. If you do not remember your username and/or password email <a href="mailto:mccloudtr@mvschool.org">Tina McCloud</a></b></font>';
+	        echo $username;
+	        echo $hashed_password;
+	        
+	        
+	        
+	    }
+	    ?>
+	</body>
+	</html>
 
 
-    } else {
-        echo '<font face="Arial, Helvetica, sans-serif"><b>The username and/or password do not match what we have in our records. Please click the BACK button on your browser and try again. If you do not remember your username and/or password email <a href="mailto:mccloudtr@mvschool.org">Tina McCloud</a></b></font>';
-        echo $username;
-        echo $hashed_password;
-        
-        
-        
-    }
-    ?>
-</body>
-</html>
 
 
 
