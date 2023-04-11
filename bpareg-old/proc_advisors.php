52975b95d582fcf5fb11e9e109dde61b9192bc28
austingere <122937592+austingere@users.noreply.github.com>
2023-04-11 16:11:22 -0500

    Update proc_advisors.php

--- bpareg-old/proc_advisors.php
+++ bpareg-old/proc_advisors.php
@@ -1,19 +1,18 @@
-<?
-
+<?php
 session_start();
-$school_number=$_SESSION['school_number'];
-$fname=$_POST['fname'];
-$lname=$_POST['lname'];
-$sphone=$_POST['sphone'];
-$cell=$_POST['cell'];
-$email=$_POST['email'];
-$size=$_POST['size'];
-$a_c=$_POST['a_c'];
-$ChapAssign=$_POST['ChapAssign'];
-$SameAssignment=$_POST['SameAssignment'];
-$AssignmentComment=$_POST['AssignmentComment'];
-$finished=$_POST['finished'];
-$addanother=$_POST['addanother'];
+$school_number = $_SESSION['school_number'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$sphone = $_POST['sphone'];
+$cell = $_POST['cell'];
+$email = $_POST['email'];
+$size = $_POST['size'];
+$a_c = $_POST['a_c'];
+$ChapAssign = $_POST['ChapAssign'];
+$SameAssignment = $_POST['SameAssignment'];
+$AssignmentComment = $_POST['AssignmentComment'];
+$finished = isset($_POST['finished']) ? true : false;
+$addanother = isset($_POST['addanother']) ? true : false;
 
 $servername = "localhost";
 $username = "bpareg";
@@ -22,11 +21,22 @@ $password = "Planetary533TrollOhm";
 $con = new PDO("mysql:host=$servername;dbname=bpareg", $username, $password);
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "INSERT INTO SLCadv (`school_number`, `fname`, `lname`, `sphone`, `cell`, `email`, `size`, `a_c`, `ChapAssign`, `SameAssignment`, `AssignmentComment`, `RegDate`, `UpdateDate`) VALUES ( '$school_number', '$fname', '$lname', '$sphone', '$cell', '$email', '$size', '$a_c', '$ChapAssign', '$SameAssignment', '$AssignmentComment', NOW(), NOW());";
-echo $sql;
-$query = $con->query($sql) or die($con->errorInfo());
+$sql = "INSERT INTO SLCadv (`school_number`, `fname`, `lname`, `sphone`, `cell`, `email`, `size`, `a_c`, `ChapAssign`, `SameAssignment`, `AssignmentComment`, `RegDate`, `UpdateDate`) VALUES ( :school_number, :fname, :lname, :sphone, :cell, :email, :size, :a_c, :ChapAssign, :SameAssignment, :AssignmentComment, NOW(), NOW());";
+$stmt = $con->prepare($sql);
+$stmt->bindParam(':school_number', $school_number);
+$stmt->bindParam(':fname', $fname);
+$stmt->bindParam(':lname', $lname);
+$stmt->bindParam(':sphone', $sphone);
+$stmt->bindParam(':cell', $cell);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':size', $size);
+$stmt->bindParam(':a_c', $a_c);
+$stmt->bindParam(':ChapAssign', $ChapAssign);
+$stmt->bindParam(':SameAssignment', $SameAssignment);
+$stmt->bindParam(':AssignmentComment', $AssignmentComment);
+
+$stmt->execute();
 
 if ($finished) { header("Location: /new/index-text-main.php?school_number=$school_number"); }
 if ($addanother) { header("Location: /new/advisors.php?school_number=$school_number"); }
-
 ?>
