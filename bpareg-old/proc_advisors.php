b281b5363dba0a437ec87af2071c73cc46db8dfa
austingere <122937592+austingere@users.noreply.github.com>
2023-04-11 16:06:42 -0500

    Update proc_advisors.php

--- bpareg-old/proc_advisors.php
+++ bpareg-old/proc_advisors.php
@@ -20,7 +20,7 @@ $username = "bpareg";
 $password = "Planetary533TrollOhm";
 
 $con = new PDO("mysql:host=$servername;dbname=bpareg", $username, $password);
-$con->setAtrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $sql = "INSERT INTO SLCadv (`school_number`, `fname`, `lname`, `sphone`, `cell`, `email`, `size`, `a_c`, `ChapAssign`, `SameAssignment`, `AssignmentComment`, `RegDate`, `UpdateDate`) VALUES ( '$school_number', '$fname', '$lname', '$sphone', '$cell', '$email', '$size', '$a_c', '$ChapAssign', '$SameAssignment', '$AssignmentComment', NOW(), NOW());";
 echo $sql
