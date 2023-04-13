cbc1445bd3ebb80ff6e9df0269033325506b96fc
mnetienne <122938286+mnetienne@users.noreply.github.com>
2023-04-13 13:59:06 -0500

    Update proc_finalize.php

--- bpareg-old/proc_finalize.php
+++ bpareg-old/proc_finalize.php
@@ -6,20 +6,16 @@ $advisors=$_POST['advisors'];
 $students=$_POST['students'];
 $chaperones=$_POST['chaperones'];
 
-
-//$con = mysql_connect('server','username','password');
-//$db = mysql_select_db('database', $con);
+//Connection to DB using PDO:
 $servername = "localhost";
 $username = "bpareg";
 $password = "Planetary533TrollOhm";
- 
 $con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
 $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 $sql = "UPDATE SLCschl SET completed='$completed', advisors='$advisors', chaperones='$chaperones', students='$students', FinalizeDate=NOW() WHERE school_number='$school_number'"; 
 
-//$query = mysql_query($sql) or die(mysql_error());
 $query = $con->query($sql) or die($con->errorInfo());
 
 header ("Location: /new/index-text-main.php?school_number=$school_number");
