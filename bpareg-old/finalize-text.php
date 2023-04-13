ba2c5cc8f54115e00705a59676c5caede97b3aae
ShaneSeessen <122937574+ShaneSeessen@users.noreply.github.com>
2023-04-13 15:17:37 -0500

    Update finalize-text.php

--- bpareg-old/finalize-text.php
+++ bpareg-old/finalize-text.php
@@ -17,31 +17,39 @@ $school_number=$_GET['school_number'];
 $servername = "localhost";
 $username = "bpareg";
 $password = "Planetary533TrollOhm";
+$dbname = "bpareg";
 
-$con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
-$con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    $stmt = $conn->prepare("SELECT * FROM SLCschl WHERE school_number = :school_number");
+    $stmt->bindParam(':school_number', $school_number);
+    $stmt->execute();
+    $school = $stmt->fetch(PDO::FETCH_BOTH);
 
-$sql = "SELECT * FROM SLCschl WHERE school_number = '$school_number'";
+    echo "
+    <html>
+    <body>
+    <font face=\"Arial, Helvetica, sans-serif\"><h1>Finalize Registration for School #$school[school_number]</font></h1>
+    <form method=\"post\" action=\"proc_finalize.php\">
+    <table>
+    <tr><td>Registration is Complete: </td><td> <input type=\"checkbox\" name=\"completed\" value=\"y\""; 
+    if ($school) { 
+        echo " checked=\"checked\""; 
+    } 
+    echo "></td></tr>\n
+    <tr><td>Number of Students Registered: </td> <td><input type=\"text\" name=\"students\" value=\"$school[students]\"></td></tr> 
+    <tr><td>Number of Advisors Registered: </td><td><input type=\"text\" name=\"advisors\" value=\"$school[advisors]\"></td></tr>
+    <tr><td>Number of Chaperones Registered: </td><td><input type=\"text\" name=\"chaperones\" value=\"$school[chaperones]\"></td></tr>
+    </table>
+    <input type=\"hidden\" name=\"school_number\" value=\"$school_number\">
+    <input type=\"hidden\" name=\"id_num\" value=\"$id_num\">
+    <input type=\"submit\" value=\"Finalize Registration\">
+    ";  
 
-$query = $con->query($sql) or die($con->errorInfo());
-$school = $query->fetch(PDO::FETCH_BOTH);
-
- echo "
-<html>
-<body>
-<font face=\"Arial, Helvetica, sans-serif\"><h1>Finalize Registration for School #$school[school_number]</font></h1>
-<form method=\"post\" action=\"proc_finalize.php\">
-<table>
-<tr><td>Registration is Complete: </td><td> <input type=\"checkbox\" name=\"completed\" value=\"y\""; if (mysqli_num_rows($school) > 0) { echo " checked=\"checked\""; } echo "></td></tr>\n
-<tr><td>Number of Students Registered: </td> <td><input type=\"text\" name=\"students\" value=\"$school[students]\"></td></tr> 
-<tr><td>Number of Advisors Registered: </td><td><input type=\"text\" name=\"advisors\" value=\"$school[advisors]\"></td></tr>
-<tr><td>Number of Chaperones Registered: </td><td><input type=\"text\" name=\"chaperones\" value=\"$school[chaperones]\"></td></tr>
-</table>
-<input type=\"hidden\" name=\"school_number\" value=\"$school_number\">
-<input type=\"hidden\" name=\"id_num\" value=\"$id_num\">
-<input type=\"submit\" value=\"Finalize Registration\">
-";  
-
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
 ?>
-
