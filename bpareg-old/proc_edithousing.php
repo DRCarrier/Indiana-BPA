a0736a4f7d3a457cb429c09fc2677898e9ca27c7
mnetienne <122938286+mnetienne@users.noreply.github.com>
2023-04-13 14:01:22 -0500

    Update proc_edithousing.php

--- bpareg-old/proc_edithousing.php
+++ bpareg-old/proc_edithousing.php
@@ -1,5 +1,5 @@
-//let syou edit housing info (connects to database)
 <?php
+//let syou edit housing info (connects to database)
 session_start();
 $room_id=$_POST['room_id'];
 $school_number=$_SESSION['school_number'];
@@ -16,19 +16,16 @@ header ("Location: /new/housinglist.php?school_number=$school_number");
 echo "<input type=\"hidden\" name=\"school_number\" value=\"$school_number\">";
 
 
-//$con = mysql_connect('server','username','password');
-//$db = mysql_select_db('database', $con);
+//Connection to DB using PDO:
 $servername = "localhost";
 $username = "bpareg";
 $password = "Planetary533TrollOhm";
- 
 $con = new PDO("mysql:host=$servername;dbname=bpareg",$username, $password );
 $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 $sql = "UPDATE housing SET a_s='$a_s', gender='$gender', type='$type', occ_id1='$occ_id1', occ_id2='$occ_id2', occ_id3='$occ_id3', occ_id4='$occ_id4' WHERE room_id = '$room_id'";
 
-//$query = mysql_query($sql) or die(mysql_error());
 $query = $con->query($sql) or die($con->errorInfo());
 
 
