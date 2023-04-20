837f012adb6e0ba91c22a069c8da1ba0293023c1
DRCarrier <113132467+DRCarrier@users.noreply.github.com>
2023-04-20 16:05:24 -0500

    Update proc_login.php

--- bpareg-old/proc_login.php
+++ bpareg-old/proc_login.php
@@ -23,7 +23,7 @@
 
     //Verify hashed password
     $password = 'password3';
-    $hashed_password = 'password_hash';
+    $hashed_password = 'password3';
     //$hashed_password = 'password';
     //$password = 'password3';
     //$h//ashed_password = 'password';
