4f54a283344a56621ad77489018f02a2ee8cf91c
DRCarrier <113132467+DRCarrier@users.noreply.github.com>
2023-04-20 16:00:44 -0500

    Update proc_login.php
    Line 26: changed //$hashed_password = 'password'; to     $hashed_password = 'pw';

--- bpareg-old/proc_login.php
+++ bpareg-old/proc_login.php
@@ -23,7 +23,8 @@
 
     //Verify hashed password
     $password = 'password3';
-    $hashed_password = 'password';
+    $hashed_password = 'pw';
+    //$hashed_password = 'password';
     //$password = 'password3';
     //$h//ashed_password = 'password';
 
